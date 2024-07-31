9af0e20f283f2db2e3df5d22b7cdad9fbefe75de
Justin Chung <20733699+justin13888@users.noreply.github.com>
2024-07-31 18:26:14 -0400

    Add PyO3 bindings WIP

--- crates/social-parser/src/lib.rs
+++ crates/social-parser/src/lib.rs
@@ -3,10 +3,28 @@ pub mod common;
 pub mod platforms;
 
 use bindings::typescript;
-
 use neon::prelude::*;
 
 #[neon::main]
 fn main(mut cx: ModuleContext) -> NeonResult<()> {
     typescript::init_context(&mut cx)
+}
+
+// ====================
+
+use pyo3::prelude::*;
+
+/// Formats the sum of two numbers as string.
+#[pyfunction]
+#[pyo3(text_signature = "(a, b) -> str")]
+fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+    Ok((a + b).to_string())
+}
+
+/// A Python module implemented in Rust.
+#[pymodule]
+fn social_parser(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    // TODO: Add the rest of the functions here
+    Ok(())
 }
